6e9da217d9f9a13af5a4ede164a4cbb66d0aca0e
Gabe Harvey <harvey7@gmail.com>
2024-04-05 13:02:27 -0500

    Added Imports and Export Obj to models/index.js

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Required Imports for Application
+const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
+
+// Exports User, Thought, and Reaction Models
+module.exports = {User, Thought, Reaction};
