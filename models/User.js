f12cfc910a106f534d0a5b6d9d687a730a839e11
Gabe Harvey <harvey7@gmail.com>
2024-04-05 13:45:12 -0500

    Added Virtual friendCount, Mongoose Model, and Export to models/User.js

--- models/User.js
+++ models/User.js
@@ -32,3 +32,14 @@ const userSchema = new Schema (
         id: false,
     }
 );
+
+// Adds Virtual friendCount to userSchema
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// Creates Mongoose Model
+const User = model('User', userSchema);
+
+// Exports User Model
+module.exports = User;
