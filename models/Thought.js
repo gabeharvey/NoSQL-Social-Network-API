7a741bfec315faee6d082c7647bd7ad21f46515b
Gabe Harvey <harvey7@gmail.com>
2024-04-05 16:29:35 -0500

    Thoughts Mongoose Schema Added to models/Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -2,3 +2,32 @@
 const { Schema, model } = require('mongoose');
 const formatDate = require('../utils/dateFormat')
 const Reaction = require('./Reaction');
+
+// Mongoose Schema for User Thoughts
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => formatDate(date)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [Reaction],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
