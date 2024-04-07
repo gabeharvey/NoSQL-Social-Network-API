ed00b08c07d4ed02e45876e477c1443bc21ebd24
Gabe Harvey <harvey7@gmail.com>
2024-04-06 22:23:11 -0500

    Added getUsers and getSingleUser Functions to contollers/user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,19 @@
 // Import Models
 const User = require('../models/User');
 const Thought = require('../models/Thought');
+
+const getUsers = (req, res) => {
+    User.find ().then((users) => res.json(users))
+    .catch((err) => res.status(500).json(err));
+};
+
+const getSingleUser = (req, res) => {
+    User.findOne ({ _id: req.params.userId }).select('-__v')
+    .populate('friends')
+    .populate('thoughts')
+    .then((user) => !user ?res.status(404).json({ message: 'Invalid Id'})
+    : res.json(user))
+    .catch((err) => res.status(500).json(err));
+};
+
+module.exports = { getUsers, getSingleUser };
