1c97cc91172a2e67c9982941e9b970ca4dcfb743
Gabe Harvey <harvey7@gmail.com>
2024-04-06 21:45:52 -0500

    Established Routers in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
 // Establish Express Router and Import Routes
 const router = require('express').Router();
 const apiRoutes = require('./api-routes');
+
+// Establish Routers
+router.use('/api', apiRoutes);
+router.use((req, res) => {
+    return res.send('Invalid Route');
+});
+
+// Export Router
+module.exports = router;
