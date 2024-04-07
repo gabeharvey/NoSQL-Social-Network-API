ef309d504f1d37b9377e43c3fb9f53f906c72d37
Gabe Harvey <harvey7@gmail.com>
2024-04-07 00:48:47 -0500

    Added Function to Start Server in server.js

--- server.js
+++ server.js
@@ -2,3 +2,27 @@
 const express = require('express');
 const routes = require('./routes');
 const db = require('./config/connection');
+
+const PORT = 3001;
+const app = express();
+const cwd = process.cwd();
+
+// Middleware for Express
+app.use(express.urlencoded ({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// This Function Starts the Server
+const startServer = async () => {
+    try {
+        await db.once ('open');
+        app.listen(PORT, () => {
+            console.log(`Server Running on PORT ${PORT}`);
+        });
+    } catch (error) {
+        console.log('Server Start Failed', error)
+    }
+};
+
+// This Calls the startServer Function
+startServer();
