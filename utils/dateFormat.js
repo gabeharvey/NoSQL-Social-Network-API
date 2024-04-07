defa5b5c012924d583f3f6fd5d4038c907c2fd2d
Gabe Harvey <harvey7@gmail.com>
2024-04-07 00:56:48 -0500

    Added Return Date Function to utils/dateFormat.js

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,2 +1,10 @@
 // Imports Date Information from Moment Library
 const moment = require('moment');
+
+// Returns Date Information
+const formatDate = (date) => {
+    return moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a");
+};
+
+// Exports Date Information
+module.exports = formatDate;
