ddb12541ba043e98c04c729dec8dfa28c60e4f8d
Gabe Harvey <harvey7@gmail.com>
2024-04-06 23:24:15 -0500

    Added getThoughts and getSingleThoughts Functions to controllers/thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,3 +1,19 @@
 // Import Models
 const User = require('../models/User');
 const Thought = require('../models/Thought');
+
+const getThoughts = (req, res) => {
+    Thought.find ().then((thoughts) => res.json(thoughts))
+    .catch((err) => res.status(500).json(err));
+};
+
+const getSingleThought = (req, res) => {
+    Thought.findOne ({ _id: req.params.thoughtId }).select('-__v')
+    .populate('reactions')
+    .then((thought) => !thought ? res.status(404).json({ message: 'No Thought Found' })
+    : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+};
+
+module.exports = { getThoughts, getSingleThought }
