0fc1ba9bdb41d1403b6922fff78474fc455ea1e8
Gabe Harvey <harvey7@gmail.com>
2024-04-06 20:23:12 -0500

    Added Routes to routes/api-routes/index.js

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -2,3 +2,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
+
+// Adds Routes to Router Object
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+// Exports Router Object
+module.exports = router;
