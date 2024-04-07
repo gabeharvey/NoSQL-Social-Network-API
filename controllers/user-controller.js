068905d635ed1a8889fd114ea197e627be55e1fa
Gabe Harvey <harvey7@gmail.com>
2024-04-06 22:35:25 -0500

    Added createUser and updateUser Functions to controllers/user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,4 +16,30 @@ const getSingleUser = (req, res) => {
     .catch((err) => res.status(500).json(err));
 };
 
-module.exports = { getUsers, getSingleUser };
+const createUser = (req, res) => {
+    User.create ({
+        username: req.body.username,
+        email: req.body.email
+    })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => res.status(500).json(err));
+};
+
+const updateUser = (req, res) => {
+    User.findOneAndUpdate (
+        { _id: req.params.userId },
+        { username: req.body.username, email: req.body.email },
+        { new: true },
+        (err, result) => {
+            if (result) {
+                res.status(200).json(result);
+                console.log(`Updated User: ${result}`);
+            } else {
+                console.log(err);
+                res.status(500).json({ message: 'User Not Updated', err });
+            }
+        }
+    );
+};
+
+module.exports = { getUsers, getSingleUser, createUser, updateUser };
