44fae71ad9a110b011b33994c692bf2a0eef9aff
Gabe Harvey <harvey7@gmail.com>
2024-04-07 00:09:18 -0500

    Added deleteThought, addReaction, removeReaction Functions to controllers/thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -51,4 +51,39 @@ const updateThought = (req, res) => {
     );
 };
 
-module.exports = { getThoughts, getSingleThought, createThought, updateThought }
+const deleteThought = (req, res) => {
+    Thought.findOneAndRemove ({ _id: req.params.thoughtId })
+    .then((thought) => !thought ? res.status(404).json({ message: 'No Thought Found' })
+    : User.findOneAndUpdate (
+        { thoughts: req.params.thoughtId },
+        { $pull: { thoughts: req.params.thoughtId } },
+        { new: true }
+    ))
+    .then((user) => !user ? res.status(404).json({ message: 'No Thought Deleted', })
+    : res.json({ message: 'Thought Deleted'}))
+    .catch((err) => res.status(500).json(err));
+};
+
+const addReaction = (req, res) => {
+    Thought.findOneAndUpdate (
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+    )
+    .then((thought) => !thought ? res.status(404).json({ message: 'No Thought Found' })
+    : res.json(`Added Reaction`))
+    .catch((err) => res.status(500).json(err));
+};
+
+const removeReaction = (req, res) => {
+    Thought.findOneAndUpdate (
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+    )
+    .then((thought) => !thought ? res.status(404).json({ message: 'No Thought Found' })
+    : res.json(`Deleted Reaction`))
+    .catch((err) => res.status(500).json(err));
+};
+
+module.exports = { getThoughts, getSingleThought, createThought, updateThought, deleteThought, addReaction, removeReaction };
