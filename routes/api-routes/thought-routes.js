f30c242a06716ceb14a624de87a0fd7260af178c
Gabe Harvey <harvey7@gmail.com>
2024-04-06 21:23:31 -0500

    Added Routes to routes/api-routes/thought-routes.js

--- routes/api-routes/thought-routes.js
+++ routes/api-routes/thought-routes.js
@@ -1,3 +1,12 @@
 // Import Controller Functions and Router Object from Express
 const router = require('express').Router();
 const { getThoughts, getSingleThought, createThought, updateThought, deleteThought, addReaction, removeReaction } = require('../../controllers/thought-controller');
+
+// Establish Routes for Controller Functions
+router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+// Export Router
+module.exports = router;
