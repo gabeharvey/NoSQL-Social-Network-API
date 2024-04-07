9954ed63552de592d727e0ea4785bca105e12499
Gabe Harvey <harvey7@gmail.com>
2024-04-06 20:55:05 -0500

    Added Controller Routes to routes/api-routes/user-routes.js

--- routes/api-routes/user-routes.js
+++ routes/api-routes/user-routes.js
@@ -1,3 +1,11 @@
 // Import Controllers and Router Object from Express
 const router = require('express').Router();
 const { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, deleteFriend } = require('../../controllers/user-controller');
+
+// Establish Routes for Controller Functions
+router.route('/').get(getUsers).post(createUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+// Export Router
+module.exports = router;
